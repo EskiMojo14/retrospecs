529c511d031cc2e6c3b0f87b717242dfa3ddc71a
Ben Durrant <ben.j.durrant@gmail.com>
2024-07-12 23:00:31 +0100

    Create basic layout for site

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import logo from "./assets/retrospecs.png";
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <img src={logo} alt="RetroSpecs" />
+        <h1>RetroSpecs</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
