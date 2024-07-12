51ea8b5fb0de3279c8fa977ae7cf68cd7829dfda
Ben Durrant <ben.j.durrant@gmail.com>
2024-07-13 00:25:23 +0100

    limit shadow to 4 lightest shades

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ function App() {
         <img src={logo} alt="RetroSpecs" />
         <h1>RetroSpecs</h1>
       </div>
+      <div className="card-demo">
+        <div className="green-card">Good</div>
+        <div className="red-card">Needs improvement</div>
+        <div className="blue-card">Neutral</div>
+        <div className="orange-card">Carry forward</div>
+      </div>
     </>
   );
 }
