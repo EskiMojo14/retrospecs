0d06a515603f23743975181f08ec7681413597c5
EskiMojo14 <ben.j.durrant@gmail.com>
2024-07-14 20:01:25 +0100

    move assets to public

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import logo from "@/assets/retrospecs.png";
+import logo from "/assets/retrospecs.png";
 import { FloatingButton } from "@/components/floating-button";
 import { Categories } from "@/features/feedback/category";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
