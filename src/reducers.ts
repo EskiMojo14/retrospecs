f7a5a1da15fcd646a45b4bf7288d8ff233f7da2c
EskiMojo14 <ben.j.durrant@gmail.com>
2024-07-14 22:07:19 +0100

    remove context slice

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,6 +1,5 @@
 import { combineSlices } from "@reduxjs/toolkit";
 import { feedbackSlice } from "@/features/feedback/slice-old";
 import { emptyApi } from "@/features/api";
-import { contextSlice } from "./features/context/slice";
 
-export const rootReducer = combineSlices(emptyApi, feedbackSlice, contextSlice);
+export const rootReducer = combineSlices(emptyApi, feedbackSlice);
