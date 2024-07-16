ab6041c2f3479f5d8870af2a8fbc3b2dc734985e
Ben Durrant <ben.durrant@marketdojo.com>
2024-07-17 00:09:17 +0100

    create ToggleButtonGroup

--- src/components/toggle-button/index.stories.tsx
+++ src/components/toggle-button/index.stories.tsx
@@ -1,20 +1,28 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-import { ToggleButton } from ".";
+import { useState, type ComponentProps, type ComponentType } from "react";
+import { ToggleButton, ToggleButtonGroup } from ".";
+
+interface StoryProps extends ComponentProps<typeof ToggleButton> {
+  dir?: string;
+}
 
 const meta = {
   title: "Components/Toggle Button",
-  component: ToggleButton,
+  component: ToggleButton as ComponentType<StoryProps>,
   parameters: {
     layout: "centered",
   },
   argTypes: {
+    dir: {
+      table: { disable: true },
+    },
     isDisabled: {
       control: "boolean",
     },
   },
   args: { onChange: fn(), isDisabled: false },
-} satisfies Meta<typeof ToggleButton>;
+} satisfies Meta<StoryProps>;
 
 export default meta;
 
@@ -24,4 +32,48 @@ export const Default: Story = {
   args: {
     children: "Button",
   },
+};
+
+function GroupComponent({ isDisabled, dir }: StoryProps) {
+  const [value, setValue] = useState("none");
+  return (
+    <ToggleButtonGroup isDisabled={isDisabled} dir={dir}>
+      <ToggleButton
+        isSelected={value === "none"}
+        onChange={() => {
+          setValue("none");
+        }}
+      >
+        None
+      </ToggleButton>
+      <ToggleButton
+        isSelected={value === "low"}
+        onChange={() => {
+          setValue("low");
+        }}
+      >
+        Low volume
+      </ToggleButton>
+      <ToggleButton
+        isSelected={value === "heavy"}
+        onChange={() => {
+          setValue("heavy");
+        }}
+      >
+        Heavy
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
+
+export const Group: Story = {
+  args: { dir: "ltr" },
+  argTypes: {
+    dir: {
+      control: "select",
+      options: ["ltr", "rtl"],
+      table: { disable: false },
+    },
+  },
+  render: (props) => <GroupComponent {...props} />,
 };
--- src/components/toggle-button/index.tsx
+++ src/components/toggle-button/index.tsx
@@ -1,5 +1,10 @@
 import { clsx } from "clsx";
-import { ToggleButton as AriaToggleButton } from "react-aria-components";
+import type { ContextType } from "react";
+import { useMemo } from "react";
+import {
+  ToggleButton as AriaToggleButton,
+  ToggleButtonContext,
+} from "react-aria-components";
 import { createGenericComponent } from "@/components/generic";
 import "./index.scss";
 
@@ -16,3 +21,38 @@ export const ToggleButton = createGenericComponent<
 >("ToggleButton", AriaToggleButton, ({ className, as: As, ...props }, ref) => (
   <As ref={ref} {...props} className={clsx("toggle-button", className)} />
 ));
+
+interface ToggleButtonGroupProps {
+  isDisabled?: boolean;
+  className?: string;
+  children: React.ReactNode;
+}
+
+export const ToggleButtonGroup = createGenericComponent<
+  "div",
+  ToggleButtonGroupProps,
+  {
+    className: string;
+    children: React.ReactNode;
+  }
+>(
+  "ToggleButtonGroup",
+  "div",
+  ({ className, as: As, children, isDisabled, ...props }, ref) => {
+    const ariaContextValue = useMemo<ContextType<typeof ToggleButtonContext>>(
+      () => ({ isDisabled }),
+      [isDisabled],
+    );
+    return (
+      <As
+        ref={ref}
+        {...props}
+        className={clsx("toggle-button-group", className)}
+      >
+        <ToggleButtonContext.Provider value={ariaContextValue}>
+          {children}
+        </ToggleButtonContext.Provider>
+      </As>
+    );
+  },
+);
