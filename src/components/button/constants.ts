acd5a00dfcf7eef62819fafa2080e50dbbad3e6a
Ben Durrant <ben.durrant@marketdojo.com>
2024-07-18 11:13:14 +0100

    change button colors

--- src/components/button/constants.ts
+++ src/components/button/constants.ts
@@ -7,7 +7,7 @@ export const buttonVariants = ["text", "outlined", "contained"] as const;
 
 export type ButtonVariant = (typeof buttonVariants)[number];
 
-export const buttonColors = ["garden", "sun", "campfire", "fridge"] as const;
+export const buttonColors = ["garden", "sun", "sunset", "dusk"] as const;
 
 export type ButtonColor = (typeof buttonColors)[number];
 
