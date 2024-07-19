c8ce06dfeb654b0b81a4d282013a7be4180704ce
Ben Durrant <ben.durrant@marketdojo.com>
2024-07-19 11:22:23 +0100

    add delete feedback confirmation dialog

--- src/features/feedback/card.tsx
+++ src/features/feedback/card.tsx
@@ -1,18 +1,20 @@
 import { clsx } from "clsx";
-import { ToggleButton } from "react-aria-components";
+import { DialogTrigger, ToggleButton } from "react-aria-components";
 import type { Feedback } from "./slice-old";
 import { feedbackAddressed, feedbackRemoved } from "./slice-old";
+import { Button } from "@/components/button";
 import {
   Card,
   CardActionIcon,
   CardActions,
   CardPrimaryAction,
 } from "@/components/card";
+import { Dialog, DialogContent } from "@/components/dialog";
 import { Symbol } from "@/components/symbol";
 import { Toolbar } from "@/components/toolbar";
+import { Heading, Typography } from "@/components/typography";
 import { useAppDispatch } from "@/pretyped";
 import styles from "./card.module.scss";
-
 interface FeedbackCardProps {
   feedback: Feedback;
 }
@@ -37,11 +39,48 @@ export function FeedbackCard({ feedback }: FeedbackCardProps) {
           <CardActionIcon onPress={console.log}>
             <Symbol>edit</Symbol>
           </CardActionIcon>
-          <CardActionIcon
-            onPress={() => dispatch(feedbackRemoved(feedback.id))}
-          >
-            <Symbol>delete</Symbol>
-          </CardActionIcon>
+          <DialogTrigger>
+            <CardActionIcon>
+              <Symbol>delete</Symbol>
+            </CardActionIcon>
+            <Dialog>
+              {({ close }) => (
+                <>
+                  <Heading variant="headline6" slot="title">
+                    Delete feedback
+                  </Heading>
+                  <DialogContent>
+                    <blockquote className={styles.quote}>
+                      {feedback.title && (
+                        <Typography variant="subtitle1">
+                          {feedback.title}
+                        </Typography>
+                      )}
+                      <Typography variant="body2">
+                        {feedback.comment}
+                      </Typography>
+                    </blockquote>
+                    <Typography variant="body1">
+                      Are you sure you want to delete this feedback?
+                    </Typography>
+                  </DialogContent>
+                  <Toolbar slot="actions">
+                    <Button onPress={close}>Cancel</Button>
+                    <Button
+                      onPress={() => {
+                        dispatch(feedbackRemoved(feedback.id));
+                        close();
+                      }}
+                      variant="elevated"
+                      color="sunset"
+                    >
+                      Delete
+                    </Button>
+                  </Toolbar>
+                </>
+              )}
+            </Dialog>
+          </DialogTrigger>
         </Toolbar>
       </CardActions>
     </Card>
