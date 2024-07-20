85115e26b19e8e62ccaeb0580b7c326bb21737ab
Ben Durrant <ben.j.durrant@gmail.com>
2024-07-20 10:55:42 +0100

    remove tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@
 import localesPlugin from "@react-aria/optimize-locales-plugin";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
     remix({
       future: {
         v3_fetcherPersist: true,
