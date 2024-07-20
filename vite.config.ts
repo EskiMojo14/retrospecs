734b55e095eca02044e1a830acde891be2ca1425
Ben Durrant <ben.j.durrant@gmail.com>
2024-07-20 16:00:36 +0100

    avoid using remix plugin in storybook

--- vite.config.ts
+++ vite.config.ts
@@ -8,17 +8,20 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
+const isStorybook = process.argv[1]?.includes("storybook");
+
 export default defineConfig({
   plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-      ignoredRouteFiles: ["**/*.{scss,css}"],
-      presets: [vercelPreset()],
-    }),
+    !isStorybook &&
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+        },
+        ignoredRouteFiles: ["**/*.{scss,css}"],
+        presets: [vercelPreset()],
+      }),
     // Don't include any locale strings in the client JS bundle.
     { ...localesPlugin.vite({ locales: [] }), enforce: "pre" },
     tsconfigPaths(),
