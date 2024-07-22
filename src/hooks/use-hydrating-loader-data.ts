e5fb956806f1c1e9ae5a2f8110ee441e2b71ae3c
Ben Durrant <ben.durrant@marketdojo.com>
2024-07-22 18:47:41 +0100

    move repetition to a separate hook

--- src/hooks/use-hydrating-loader-data.ts
+++ src/hooks/use-hydrating-loader-data.ts
@@ -1,18 +1,23 @@
 import { useLoaderData, useRouteLoaderData } from "@remix-run/react";
 import type { SerializeFrom } from "@remix-run/server-runtime";
 import { useRef } from "react";
+import type { RootState } from "~/store";
 import type { HydratingLoader } from "~/store/hydrate";
 import { hydrate } from "~/store/hydrate";
 import { useAppDispatch } from "~/store/pretyped";
 
-export function useHydratingLoaderData<T extends HydratingLoader<unknown>>() {
-  const { state, data } = useLoaderData<T>();
+function useHydrate(state: RootState | SerializeFrom<RootState> | undefined) {
   const dispatch = useAppDispatch();
   const hydrated = useRef(false);
-  if (!hydrated.current) {
+  if (!hydrated.current && state) {
     dispatch(hydrate(state));
     hydrated.current = true;
   }
+}
+
+export function useHydratingLoaderData<T extends HydratingLoader<unknown>>() {
+  const { state, data } = useLoaderData<T>();
+  useHydrate(state);
   return data as SerializeFrom<T extends HydratingLoader<infer U> ? U : never>;
 }
 
@@ -20,13 +25,9 @@ export function useHydratingRouteLoaderData<T extends HydratingLoader<unknown>>(
   route: string,
 ) {
   const routeLoaderData = useRouteLoaderData<T>(route);
-  const dispatch = useAppDispatch();
-  const hydrated = useRef(false);
-  if (!routeLoaderData) return undefined;
-  const { state, data } = routeLoaderData;
-  if (!hydrated.current) {
-    dispatch(hydrate(state));
-    hydrated.current = true;
-  }
-  return data as SerializeFrom<T extends HydratingLoader<infer U> ? U : never>;
+  const { state, data } = routeLoaderData ?? {};
+  useHydrate(state);
+  return data as
+    | SerializeFrom<T extends HydratingLoader<infer U> ? U : never>
+    | undefined;
 }
