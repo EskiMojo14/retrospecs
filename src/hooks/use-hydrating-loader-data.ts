71d2ca62510248c1ce4f127a14ab896dae4bfc25
Ben Durrant <ben.durrant@marketdojo.com>
2024-07-22 16:58:13 +0100

    create route loader version just in case

--- src/hooks/use-hydrating-loader-data.ts
+++ src/hooks/use-hydrating-loader-data.ts
@@ -1,4 +1,4 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useRouteLoaderData } from "@remix-run/react";
 import type { SerializeFrom } from "@remix-run/server-runtime";
 import { useRef } from "react";
 import type { HydratingLoader } from "~/store/hydrate";
@@ -9,6 +9,21 @@ export function useHydratingLoaderData<T extends HydratingLoader<unknown>>() {
   const { state, data } = useLoaderData<T>();
   const dispatch = useAppDispatch();
   const hydrated = useRef(false);
+  if (!hydrated.current) {
+    dispatch(hydrate(state));
+    hydrated.current = true;
+  }
+  return data as SerializeFrom<T extends HydratingLoader<infer U> ? U : never>;
+}
+
+export function useHydratingRouteLoaderData<T extends HydratingLoader<unknown>>(
+  route: string,
+) {
+  const routeLoaderData = useRouteLoaderData<T>(route);
+  const dispatch = useAppDispatch();
+  const hydrated = useRef(false);
+  if (!routeLoaderData) return undefined;
+  const { state, data } = routeLoaderData;
   if (!hydrated.current) {
     dispatch(hydrate(state));
     hydrated.current = true;
