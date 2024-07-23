e517b90794a866eca8f665e9c8f7c13242659f4d
Ben Durrant <ben.durrant@marketdojo.com>
2024-07-23 13:52:25 +0100

    fix redirect

--- app/routes/sign-in/route.tsx
+++ app/routes/sign-in/route.tsx
@@ -14,6 +14,19 @@ import styles from "./route.module.scss";
 
 export const meta: MetaFunction = () => [{ title: "RetroSpecs - Sign in" }];
 
+const getURL = () => {
+  if (typeof process === "undefined") return "http://localhost:3000/";
+  let url =
+    process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url;
+};
+
 export default function SignIn() {
   const supabase = useSupabase();
   const [searchParams, setSearchParams] = useSearchParams();
@@ -58,7 +71,7 @@ export default function SignIn() {
           void supabase.auth.signInWithOAuth({
             provider: "github",
             options: {
-              redirectTo: `${window.location.origin}/auth/callback`,
+              redirectTo: `${getURL()}auth/callback`,
             },
           });
         }}
