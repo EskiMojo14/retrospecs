b953c3c80e7316c1b65b2dc096725023eb0e5a73
Ben Durrant <ben.durrant@marketdojo.com>
2024-07-25 12:35:03 +0100

    move org create dialog to own file

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,24 +1,18 @@
 import type { MetaFunction } from "@remix-run/react";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import type { FormEvent } from "react";
-import { DialogTrigger, Form, Text } from "react-aria-components";
-import type { BaseSchema } from "valibot";
-import { minLength, object, pipe, safeParse, string } from "valibot";
+import { useQuery } from "@tanstack/react-query";
+import { DialogTrigger, Text } from "react-aria-components";
 import { AppBar } from "~/components/app-bar";
 import { Breadcrumb, Breadcrumbs } from "~/components/breadcrumbs";
-import { Button, FloatingActionButton, LinkButton } from "~/components/button";
-import { Dialog, DialogContent } from "~/components/dialog";
+import { FloatingActionButton, LinkButton } from "~/components/button";
 import { IconButton } from "~/components/icon-button";
-import { TextField } from "~/components/input/text-field";
 import { LineBackground } from "~/components/line-background";
 import { Link } from "~/components/link";
 import { Symbol } from "~/components/symbol";
 import { Toolbar } from "~/components/toolbar";
-import { Heading } from "~/components/typography";
 import { createHydratingLoader } from "~/db/query";
-import type { TablesInsert } from "~/db/supabase";
 import { Logo } from "~/features/logo";
-import { addOrg, getOrgs, selectOrgIds } from "~/features/orgs";
+import { getOrgs, selectOrgIds } from "~/features/orgs";
+import { CreateOrg } from "~/features/orgs/create-org";
 import { OrgGrid } from "~/features/orgs/org-grid";
 import { PreferencesDialog } from "~/features/user_config/dialog";
 import { useOptionsCreator } from "~/hooks/use-options-creator";
@@ -38,33 +32,11 @@ export const loader = createHydratingLoader(
   },
 );
 
-const createOrgSchema = object({
-  name: pipe(string(), minLength(1)),
-}) satisfies BaseSchema<any, TablesInsert<"orgs">, any>;
-
 export default function Orgs() {
   const { data: orgIds = [] } = useQuery({
     ...useOptionsCreator(getOrgs),
     select: selectOrgIds,
   });
-  const {
-    mutate: addOrgFn,
-    isError,
-    isPending,
-  } = useMutation(useOptionsCreator(addOrg));
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const unparsedData = new FormData(event.currentTarget);
-    const parsedData = safeParse(
-      createOrgSchema,
-      Object.fromEntries(unparsedData),
-    );
-    if (parsedData.success) {
-      addOrgFn(parsedData.output);
-    } else {
-      console.error(parsedData.issues);
-    }
-  };
 
   return (
     <main>
@@ -91,36 +63,7 @@ export default function Orgs() {
             <Symbol slot="leading">add</Symbol>
             <Text slot="label">Create</Text>
           </FloatingActionButton>
-          <Dialog>
-            {({ close }) => (
-              <>
-                <Heading variant="headline6" slot="title">
-                  Create organisation
-                </Heading>
-                <DialogContent
-                  as={Form}
-                  id="create-org-form"
-                  onSubmit={handleSubmit}
-                >
-                  <TextField label="Name" name="name" isRequired />
-                </DialogContent>
-                <Toolbar slot="actions">
-                  <Button onPress={close} variant="outlined">
-                    Cancel
-                  </Button>
-                  <Button
-                    type="submit"
-                    form="create-org-form"
-                    variant="elevated"
-                    isDisabled={isPending}
-                    color={isError ? "red" : undefined}
-                  >
-                    Create
-                  </Button>
-                </Toolbar>
-              </>
-            )}
-          </Dialog>
+          <CreateOrg />
         </DialogTrigger>
       </LineBackground>
     </main>
---
+++ src/features/orgs/create-org.tsx
@@ -0,0 +1,66 @@
+import { useMutation } from "@tanstack/react-query";
+import type { FormEvent } from "react";
+import { Form } from "react-aria-components";
+import type { BaseSchema } from "valibot";
+import { minLength, object, pipe, safeParse, string } from "valibot";
+import { Button } from "~/components/button";
+import { Dialog, DialogContent } from "~/components/dialog";
+import { TextField } from "~/components/input/text-field";
+import { Toolbar } from "~/components/toolbar";
+import { Heading } from "~/components/typography";
+import type { TablesInsert } from "~/db/supabase";
+import { useOptionsCreator } from "~/hooks/use-options-creator";
+import { addOrg } from ".";
+
+const createOrgSchema = object({
+  name: pipe(string(), minLength(1)),
+}) satisfies BaseSchema<any, TablesInsert<"orgs">, any>;
+
+export function CreateOrg() {
+  const {
+    mutate: addOrgFn,
+    isError,
+    isPending,
+  } = useMutation(useOptionsCreator(addOrg));
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const unparsedData = new FormData(event.currentTarget);
+    const parsedData = safeParse(
+      createOrgSchema,
+      Object.fromEntries(unparsedData),
+    );
+    if (parsedData.success) {
+      addOrgFn(parsedData.output);
+    } else {
+      console.error(parsedData.issues);
+    }
+  };
+  return (
+    <Dialog>
+      {({ close }) => (
+        <>
+          <Heading variant="headline6" slot="title">
+            Create organisation
+          </Heading>
+          <DialogContent as={Form} id="create-org-form" onSubmit={handleSubmit}>
+            <TextField label="Name" name="name" isRequired />
+          </DialogContent>
+          <Toolbar slot="actions">
+            <Button onPress={close} variant="outlined">
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              form="create-org-form"
+              variant="elevated"
+              isDisabled={isPending}
+              color={isError ? "red" : undefined}
+            >
+              Create
+            </Button>
+          </Toolbar>
+        </>
+      )}
+    </Dialog>
+  );
+}
