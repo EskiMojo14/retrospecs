f48d3b231cb1b021274fb9bf8d762d79f1fa8b47
Ben Durrant <ben.durrant@marketdojo.com>
2024-07-26 02:30:44 +0100

    clamp toast time to a minimum of 5s

--- src/components/toast/index.tsx
+++ src/components/toast/index.tsx
@@ -2,7 +2,17 @@ import { ToastQueue } from "@react-stately/toast";
 import type { Toast } from "./constants";
 import "./index.scss";
 
-export const toastQueue = new ToastQueue<Toast>({
+class ClampedToastQueue<T> extends ToastQueue<T> {
+  add(...[toast, config]: Parameters<ToastQueue<T>["add"]>) {
+    if (config?.timeout && config.timeout < 5000) {
+      config.timeout = 5000;
+    }
+
+    return super.add(toast, config);
+  }
+}
+
+export const toastQueue = new ClampedToastQueue<Toast>({
   maxVisibleToasts: 5,
   hasExitAnimation: true,
 });
