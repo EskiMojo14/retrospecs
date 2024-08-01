141c1a87a6441e4c1b43ca3e5bd15c56c2a107a5
Ben Durrant <ben.durrant@marketdojo.com>
2024-08-01 13:31:17 +0100

    last one

--- app/routes/env.tsx
+++ app/routes/env.tsx
@@ -1,16 +1,13 @@
 import { useLoaderData } from "@remix-run/react";
 
-export const loader = () => process.env;
+export const loader = () => import.meta.env;
 
 export default function Env() {
   const env = useLoaderData<typeof loader>();
-  const clientEnv = process.env;
-  const importMetaEnv = import.meta.env;
   return (
     <>
       <pre>{JSON.stringify(env, null, 2)}</pre>
-      <pre>{JSON.stringify(clientEnv, null, 2)}</pre>
-      <pre>{JSON.stringify(importMetaEnv, null, 2)}</pre>
+      <pre>{JSON.stringify(import.meta.env, null, 2)}</pre>
     </>
   );
 }
