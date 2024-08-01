52e99ab1b34947d909e85d54ff19c8d695902ab9
Ben Durrant <ben.durrant@marketdojo.com>
2024-08-01 13:13:16 +0100

    try all the things

--- app/routes/sign-in/route.tsx
+++ app/routes/sign-in/route.tsx
@@ -17,7 +17,9 @@ export const meta: MetaFunction = () => [{ title: "RetroSpecs - Sign in" }];
 const getURL = () => {
   if (typeof process === "undefined") return "http://localhost:5173/";
   let url =
+    process.env.SITE_URL ??
     process.env.VITE_SITE_URL ?? // Set this to your site URL in production env.
+    process.env.VERCEL_URL ?? // Automatically set by Vercel.
     process.env.VITE_VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:5173/";
   // Make sure to include `https://` when not localhost.
