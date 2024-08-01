25b5ef8b40ad4924ad00c279f001846de89e43c4
Ben Durrant <ben.durrant@marketdojo.com>
2024-08-01 13:26:54 +0100

    include import meta env

--- app/routes/env.tsx
+++ app/routes/env.tsx
@@ -5,10 +5,12 @@ export const loader = () => process.env;
 export default function Env() {
   const env = useLoaderData<typeof loader>();
   const clientEnv = process.env;
+  const importMetaEnv = import.meta.env;
   return (
     <>
       <pre>{JSON.stringify(env, null, 2)}</pre>
       <pre>{JSON.stringify(clientEnv, null, 2)}</pre>
+      <pre>{JSON.stringify(importMetaEnv, null, 2)}</pre>
     </>
   );
 }
