ac23915eaf52a06c229dcaaf96bc2cf11a0618e7
Ben Durrant <ben.j.durrant@gmail.com>
2024-08-10 01:38:22 +0100

    separate out create team dialog

--- app/routes/orgs_.$orgId/route.tsx
+++ app/routes/orgs_.$orgId/route.tsx
@@ -1,33 +1,21 @@
 import type { MetaFunction } from "@remix-run/react";
 import { useLoaderData, useParams } from "@remix-run/react";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { useRef, type FormEvent } from "react";
-import { Form } from "react-aria-components";
-import type { BaseSchema } from "valibot";
-import { minLength, number, object, pipe, string } from "valibot";
-import { Button } from "~/components/button";
+import { useQuery } from "@tanstack/react-query";
 import { ExtendedFab } from "~/components/button/fab";
-import { Dialog, DialogContent } from "~/components/dialog";
-import { TextField } from "~/components/input/text-field";
 import { LineBackground } from "~/components/line-background";
 import { Symbol } from "~/components/symbol";
-import { Toolbar } from "~/components/toolbar";
-import { Heading } from "~/components/typography";
 import { ensureCurrentUserPermissions } from "~/db/auth.server";
 import { createHydratingLoader } from "~/db/loader.server";
-import { useSession } from "~/db/provider";
-import type { TablesInsert } from "~/db/supabase";
 import { NavBar } from "~/features/nav-bar";
 import { getOrg } from "~/features/orgs";
 import { getSprintCountForTeam } from "~/features/sprints";
 import {
-  addTeam,
   getTeamMemberCount,
   getTeamsByOrg,
   selectTeamIds,
 } from "~/features/teams";
+import { CreateTeam } from "~/features/teams/create-team";
 import { TeamGrid } from "~/features/teams/team-grid";
-import { useFormSchema } from "~/hooks/use-form-schema";
 import { useOptionsCreator } from "~/hooks/use-options-creator";
 
 export const meta: MetaFunction<any> = ({
@@ -66,15 +54,7 @@ export const loader = createHydratingLoader(
   },
 );
 
-const createTeamSchema = object({
-  name: pipe(string(), minLength(1)),
-  org_id: number(),
-  created_by: string(),
-}) satisfies BaseSchema<any, TablesInsert<"teams">, any>;
-
 export default function Org() {
-  const formRef = useRef<HTMLFormElement>(null);
-  const session = useSession();
   const params = useParams();
   const orgId = Number(params.orgId);
   const loaderData = useLoaderData<typeof loader>();
@@ -87,14 +67,6 @@ export default function Org() {
     initialData: loaderData.teams,
     select: selectTeamIds,
   });
-  const {
-    mutate: addTeamFn,
-    isError,
-    isPending,
-    reset,
-  } = useMutation(useOptionsCreator(addTeam));
-  const [errors, validateTeam, resetValidation] =
-    useFormSchema(createTeamSchema);
   return (
     <main>
       <LineBackground opacity={0.5}>
@@ -105,7 +77,7 @@ export default function Org() {
           ]}
         />
         <TeamGrid orgId={orgId} teamIds={teamIds} />
-        <Dialog
+        <CreateTeam
           trigger={
             <ExtendedFab
               color="green"
@@ -116,59 +88,8 @@ export default function Org() {
               Create
             </ExtendedFab>
           }
-          triggerProps={{
-            onOpenChange: (isOpen) => {
-              if (!isOpen) {
-                formRef.current?.reset();
-                reset();
-                resetValidation();
-              }
-            },
-          }}
-        >
-          {({ close }) => (
-            <>
-              <Heading variant="headline6" slot="title">
-                Create team
-              </Heading>
-              <DialogContent
-                as={Form}
-                id="create-team-form"
-                onSubmit={(event: FormEvent<HTMLFormElement>) => {
-                  event.preventDefault();
-                  const unparsedData = new FormData(event.currentTarget);
-                  const parsedData = validateTeam({
-                    ...Object.fromEntries(unparsedData),
-                    org_id: orgId,
-                    created_by: session?.user.id,
-                  });
-                  if (parsedData.success) {
-                    addTeamFn(parsedData.output, {
-                      onSuccess: close,
-                    });
-                  }
-                }}
-                validationErrors={errors?.validationErrors}
-              >
-                <TextField label="Name" name="name" isRequired />
-              </DialogContent>
-              <Toolbar slot="actions">
-                <Button onPress={close} variant="outlined">
-                  Cancel
-                </Button>
-                <Button
-                  type="submit"
-                  form="create-team-form"
-                  variant="elevated"
-                  isDisabled={isPending}
-                  color={isError ? "red" : undefined}
-                >
-                  Create
-                </Button>
-              </Toolbar>
-            </>
-          )}
-        </Dialog>
+          orgId={orgId}
+        />
       </LineBackground>
     </main>
   );
---
+++ src/features/teams/create-team.tsx
@@ -0,0 +1,97 @@
+import { mergeProps } from "@react-aria/utils";
+import { useMutation } from "@tanstack/react-query";
+import { useRef, type FormEvent } from "react";
+import { Form } from "react-aria-components";
+import type { BaseSchema } from "valibot";
+import { minLength, number, object, pipe, string } from "valibot";
+import { Button } from "~/components/button";
+import type { DialogProps } from "~/components/dialog";
+import { Dialog, DialogContent } from "~/components/dialog";
+import { TextField } from "~/components/input/text-field";
+import { Toolbar } from "~/components/toolbar";
+import { Heading } from "~/components/typography";
+import { useSession } from "~/db/provider";
+import type { TablesInsert } from "~/db/supabase";
+import { useFormSchema } from "~/hooks/use-form-schema";
+import { useOptionsCreator } from "~/hooks/use-options-creator";
+import { addTeam } from ".";
+
+export interface CreateTeamProps extends Omit<DialogProps, "children"> {
+  orgId: number;
+}
+
+const createTeamSchema = object({
+  name: pipe(string(), minLength(1)),
+  org_id: number(),
+  created_by: string(),
+}) satisfies BaseSchema<any, TablesInsert<"teams">, any>;
+
+export function CreateTeam({ orgId, triggerProps, ...props }: CreateTeamProps) {
+  const formRef = useRef<HTMLFormElement>(null);
+  const session = useSession();
+  const {
+    mutate: addTeamFn,
+    isError,
+    isPending,
+    reset,
+  } = useMutation(useOptionsCreator(addTeam));
+  const [errors, validateTeam, resetValidation] =
+    useFormSchema(createTeamSchema);
+  return (
+    <Dialog
+      {...props}
+      triggerProps={mergeProps(triggerProps, {
+        onOpenChange: (isOpen: boolean) => {
+          if (!isOpen) {
+            formRef.current?.reset();
+            reset();
+            resetValidation();
+          }
+        },
+      })}
+    >
+      {({ close }) => (
+        <>
+          <Heading variant="headline6" slot="title">
+            Create team
+          </Heading>
+          <DialogContent
+            as={Form}
+            id="create-team-form"
+            onSubmit={(event: FormEvent<HTMLFormElement>) => {
+              event.preventDefault();
+              const unparsedData = new FormData(event.currentTarget);
+              const parsedData = validateTeam({
+                ...Object.fromEntries(unparsedData),
+                org_id: orgId,
+                created_by: session?.user.id,
+              });
+              if (parsedData.success) {
+                addTeamFn(parsedData.output, {
+                  onSuccess: close,
+                });
+              }
+            }}
+            validationErrors={errors?.validationErrors}
+          >
+            <TextField label="Name" name="name" isRequired />
+          </DialogContent>
+          <Toolbar slot="actions">
+            <Button onPress={close} variant="outlined">
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              form="create-team-form"
+              variant="elevated"
+              isDisabled={isPending}
+              color={isError ? "red" : undefined}
+            >
+              Create
+            </Button>
+          </Toolbar>
+        </>
+      )}
+    </Dialog>
+  );
+}
