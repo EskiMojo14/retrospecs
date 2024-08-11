50df6b7b1bea2337de6eedc2a6cc9ddca01f492b
EskiMojo14 <ben.j.durrant@gmail.com>
2024-08-11 18:51:21 +0100

    improve avatar group story

--- src/components/avatar/index.stories.tsx
+++ src/components/avatar/index.stories.tsx
@@ -1,11 +1,22 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import type { ComponentPropsWithoutRef, ComponentType } from "react";
-import { Button } from "~/components/button";
+import {
+  useState,
+  type ComponentPropsWithoutRef,
+  type ComponentType,
+} from "react";
+import { Dialog, DialogTrigger } from "react-aria-components";
+import { Button, ButtonGroup, ToggleButton } from "~/components/button";
+import { List, ListItem, ListItemText } from "~/components/list";
+import { Popover } from "~/components/popover";
+import { Symbol } from "~/components/symbol";
 import { Tooltip, TooltipTrigger } from "~/components/tooltip";
+import { Heading } from "~/components/typography";
 import type { Color } from "~/theme/colors";
 import { colors } from "~/theme/colors";
+import { pluralize } from "~/util";
 import type { AvatarGroupProps } from ".";
 import { Avatar, AvatarGroup } from ".";
+import styles from "./story.module.scss";
 
 export interface StoryProps
   extends ComponentPropsWithoutRef<typeof Avatar>,
@@ -62,10 +73,53 @@ const examples: Array<{ name: string; color: Color; src?: string }> = [
     src: "https://avatars.githubusercontent.com/u/176902558?v=4",
   },
   { name: "Harry", color: "green" },
-  { name: "Jeremy", color: "amber" },
-  { name: "Bertha", color: "teal" },
-  { name: "Charles", color: "gold" },
 ];
+
+function LikeButton({ children, ...props }: StoryProps) {
+  const [liked, setLiked] = useState(false);
+  const likedBy = liked ? examples : examples.slice(1);
+  return (
+    <ButtonGroup id="like-button" variant="outlined">
+      <ToggleButton
+        onChange={(liked) => {
+          setLiked(liked);
+        }}
+      >
+        <Symbol slot="trailing">thumb_up</Symbol>
+        {pluralize`${likedBy.length} ${[likedBy.length, "like"]}`}
+      </ToggleButton>
+      <TooltipTrigger>
+        <DialogTrigger>
+          <Button>
+            <AvatarGroup {...props} items={likedBy}>
+              {(props) => <Avatar id={props.name} {...props} />}
+            </AvatarGroup>
+          </Button>
+          <Popover placement="bottom" className={styles.popover}>
+            <Dialog className={styles.dialog}>
+              <Heading
+                variant="subtitle1"
+                slot="title"
+                className={styles.title}
+              >
+                Reactions
+              </Heading>
+              <List variant="one-line" items={likedBy} className={styles.list}>
+                {(props) => (
+                  <ListItem id={props.name}>
+                    <Avatar {...props} />
+                    <ListItemText headline={props.name} />
+                  </ListItem>
+                )}
+              </List>
+            </Dialog>
+          </Popover>
+        </DialogTrigger>
+        <Tooltip>Reactions from {liked ? "Tom, " : ""}Dick, and 4 more</Tooltip>
+      </TooltipTrigger>
+    </ButtonGroup>
+  );
+}
 
 export const Group: Story = {
   argTypes: {
@@ -84,20 +138,13 @@ export const Group: Story = {
       options: ["small", "medium", "large"],
       table: { disable: false },
     },
+    size: { table: { disable: true } },
+    color: { table: { disable: true } },
   },
-  render: ({ font, color, size, spacing, max }) => (
-    <TooltipTrigger>
-      <Button>
-        <AvatarGroup {...{ font, color, size, spacing, max }} items={examples}>
-          {(props) => <Avatar id={props.name} {...props} />}
-        </AvatarGroup>
-      </Button>
-      <Tooltip>Reactions from Tom, Dick, and 4 more</Tooltip>
-    </TooltipTrigger>
-  ),
+  render: (props) => <LikeButton {...props} />,
   args: {
     spacing: "medium",
     size: "x-small",
-    max: 5,
+    max: 3,
   },
 };
