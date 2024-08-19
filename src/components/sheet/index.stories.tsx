eb39c10257ef032192f10f43bf75474210a1190f
Ben Durrant <ben.j.durrant@gmail.com>
2024-08-19 01:47:03 +0100

    create modal side sheet component

---
+++ src/components/sheet/index.stories.tsx
@@ -0,0 +1,86 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Header } from "react-aria-components";
+import { Button } from "~/components/button";
+import { Checkbox } from "~/components/checkbox";
+import { Divider } from "~/components/divider";
+import { IconButton } from "~/components/icon-button";
+import { List, ListItem } from "~/components/list";
+import { Symbol } from "~/components/symbol";
+import { Toolbar } from "~/components/toolbar";
+import { Heading } from "~/components/typography";
+import { ModalSideSheet, SheetContent } from ".";
+
+const meta = {
+  title: "Components/SideSheet",
+  render: (props) => (
+    <ModalSideSheet {...props}>
+      {({ close }) => (
+        <>
+          <Header slot="title">
+            <Heading variant="headline6" slot="title">
+              Filters
+            </Heading>
+            <IconButton onPress={close} tooltip="Close">
+              <Symbol>close</Symbol>
+            </IconButton>
+          </Header>
+          <SheetContent>
+            <section>
+              <Heading
+                variant="subtitle2"
+                style={{ margin: 0, padding: "0 24px" }}
+                id="label-title"
+              >
+                Labels
+              </Heading>
+              <List
+                variant="one-line"
+                selectionMode="multiple"
+                aria-labelledby="label-title"
+              >
+                {["Events", "Personal", "Projects", "Reminders", "Family"].map(
+                  (category) => (
+                    <ListItem key={category} textValue={category}>
+                      <Checkbox slot="selection" />
+                      {category}
+                    </ListItem>
+                  ),
+                )}
+              </List>
+            </section>
+          </SheetContent>
+          <Divider />
+          <Toolbar slot="actions">
+            <Button onPress={close} variant="filled">
+              Save
+            </Button>
+            <Button onPress={close} variant="outlined">
+              Cancel
+            </Button>
+          </Toolbar>
+        </>
+      )}
+    </ModalSideSheet>
+  ),
+  argTypes: {
+    trigger: { table: { disable: true } },
+    attach: {
+      control: "inline-radio",
+      options: ["start", "end"],
+    },
+  },
+  args: {
+    isDismissable: false,
+    isKeyboardDismissDisabled: false,
+    trigger: <Button>Open sheet</Button>,
+    attach: "end",
+  },
+} satisfies Meta<typeof ModalSideSheet>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
---
+++ src/components/sheet/index.tsx
@@ -0,0 +1,137 @@
+import type { ContextType, ReactNode } from "react";
+import type {
+  DialogProps as AriaDialogProps,
+  DialogTriggerProps,
+  ModalOverlayProps,
+} from "react-aria-components";
+import {
+  DEFAULT_SLOT,
+  Dialog,
+  DialogTrigger,
+  HeaderContext,
+  HeadingContext,
+  ModalOverlay,
+  Modal,
+} from "react-aria-components";
+import { createGenericComponent } from "~/components/generic";
+import { LineBackground } from "~/components/line-background";
+import { MergeProvider, Provider } from "~/components/provider";
+import { ToolbarContext } from "~/components/toolbar";
+import { bemHelper, renderPropsChild } from "~/util";
+import "./index.scss";
+
+export interface ModalSideSheetProps
+  extends AriaDialogProps,
+    Pick<
+      ModalOverlayProps,
+      | "isDismissable"
+      | "isKeyboardDismissDisabled"
+      | "shouldCloseOnInteractOutside"
+    > {
+  modalProps?: ModalOverlayProps & { className?: string };
+  modalOverlayProps?: ModalOverlayProps & { className?: string };
+  trigger: ReactNode;
+  triggerProps?: Omit<DialogTriggerProps, "children">;
+  /** @default "end" */
+  attach?: "start" | "end";
+}
+
+const cls = bemHelper("sheet");
+const overlayCls = bemHelper("sheet-overlay");
+
+const toolbarContextValue: ContextType<typeof ToolbarContext> = {
+  slots: {
+    [DEFAULT_SLOT]: {},
+    actions: { align: "start", className: cls("actions") },
+  },
+};
+
+const headerContextValue: ContextType<typeof HeaderContext> = {
+  slots: {
+    [DEFAULT_SLOT]: {},
+    title: {
+      className: cls("title-container"),
+    },
+  },
+};
+
+const headingContextValue: ContextType<typeof HeadingContext> = {
+  slots: {
+    title: {
+      className: cls("title"),
+    },
+  },
+};
+
+export const ModalSideSheet = ({
+  className,
+  modalOverlayProps,
+  modalProps,
+  isDismissable,
+  isKeyboardDismissDisabled,
+  shouldCloseOnInteractOutside,
+  children,
+  trigger,
+  triggerProps,
+  attach = "end",
+  ...props
+}: ModalSideSheetProps) => (
+  <DialogTrigger {...triggerProps}>
+    {trigger}
+    <ModalOverlay
+      {...{
+        isDismissable,
+        isKeyboardDismissDisabled,
+        shouldCloseOnInteractOutside,
+      }}
+      {...modalOverlayProps}
+      className={overlayCls({ extra: modalOverlayProps?.className })}
+    >
+      <Modal
+        {...modalProps}
+        className={cls({
+          modifiers: ["modal", `attach-${attach}`],
+          extra: modalProps?.className,
+        })}
+      >
+        <LineBackground opacity={0.3}>
+          <Dialog
+            {...props}
+            className={cls({
+              element: "dialog",
+              extra: className,
+            })}
+          >
+            {renderPropsChild(children, (children) => (
+              <Provider
+                values={[
+                  [ToolbarContext, toolbarContextValue],
+                  [HeaderContext, headerContextValue],
+                ]}
+              >
+                <MergeProvider
+                  context={HeadingContext}
+                  value={headingContextValue}
+                >
+                  {children}
+                </MergeProvider>
+              </Provider>
+            ))}
+          </Dialog>
+        </LineBackground>
+      </Modal>
+    </ModalOverlay>
+  </DialogTrigger>
+);
+
+export const SheetContent = createGenericComponent<
+  "section",
+  { className?: string },
+  { className: string }
+>("SheetContent", "section", ({ as: As, className, ...props }, ref) => (
+  <As
+    {...props}
+    ref={ref}
+    className={cls({ element: "content", extra: className })}
+  />
+));
