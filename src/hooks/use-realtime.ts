9f5f599c6def136282c21784bc736eefc00e6205
EskiMojo14 <ben.j.durrant@gmail.com>
2024-09-01 23:48:12 +0100

    make sure each realtime handler only makes one channel

--- src/hooks/use-realtime.ts
+++ src/hooks/use-realtime.ts
@@ -2,15 +2,34 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useSupabase } from "~/db/provider";
 import type { Unsubscribable } from "~/util";
+import { mapEmplace } from "~/util/ponyfills";
 import type { AppContext } from "~/util/supabase-query";
+
+const handlerCache = new WeakMap<
+  (context: AppContext) => Unsubscribable,
+  { channel: Unsubscribable; count: number }
+>();
 
 export function useRealtime(handler: (context: AppContext) => Unsubscribable) {
   const supabase = useSupabase();
   const queryClient = useQueryClient();
   useEffect(() => {
-    const channel = handler({ supabase, queryClient });
+    const entry = mapEmplace(handlerCache, handler, {
+      insert: () => ({
+        channel: handler({ supabase, queryClient }),
+        count: 1,
+      }),
+      update: (entry) => {
+        entry.count++;
+        return entry;
+      },
+    });
     return () => {
-      channel.unsubscribe();
+      entry.count--;
+      if (entry.count === 0) {
+        entry.channel.unsubscribe();
+        handlerCache.delete(handler);
+      }
     };
   }, [supabase, queryClient, handler]);
 }
