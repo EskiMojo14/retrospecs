4c628b85f7df62bc15af5c44d85442f6d84e8609
Ben Durrant <ben.durrant@marketdojo.com>
2024-09-09 15:18:19 +0100

    add remix devtools

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,31 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { vercelPreset } from "@vercel/remix/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { remixDevTools } from "remix-development-tools";
 
 const isStorybook = process.argv[1]?.includes("storybook");
 
 export default defineConfig({
   plugins: [
-    !isStorybook &&
-      remix({
-        future: {
-          v3_fetcherPersist: true,
-          v3_relativeSplatPath: true,
-          v3_throwAbortReason: true,
-          unstable_singleFetch: true,
-        },
-        ignoredRouteFiles: ["**/*.{scss,css}"],
-        presets: [vercelPreset()],
-      }),
+    ...(!isStorybook
+      ? [
+          remixDevTools({
+            client: {
+              position: "middle-left",
+            },
+          }),
+          remix({
+            future: {
+              v3_fetcherPersist: true,
+              v3_relativeSplatPath: true,
+              v3_throwAbortReason: true,
+              unstable_singleFetch: true,
+            },
+            ignoredRouteFiles: ["**/*.{scss,css}"],
+            presets: [vercelPreset()],
+          }),
+        ]
+      : []),
     // Don't include any locale strings in the client JS bundle.
     { ...localesPlugin.vite({ locales: [] }), enforce: "pre" },
     tsconfigPaths(),
